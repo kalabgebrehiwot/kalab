5423c92af91d893bb5d2c839fb55b3216f9c73e3
kalabgebrehiwot <154657840+kalabgebrehiwot@users.noreply.github.com>
2024-04-19 13:33:34 +0300

    building a calorie

---
+++ building a calorie counter/an.js
@@ -0,0 +1,14 @@
+let isError = false;
+const calorieCounter = document.getElementById("calorie-counter");
+const budgetNumberInput = document.getElementById("budget");
+const entryDropdown = document.getElementById("entry-dropdown");
+const addEntryButton = document.getElementById("add-entry");
+const clearButton = document.getElementById("clear");
+const output = document.getElementById("output");
+function cleanInputString(str) {
+  const regex = /[+-\s]/g;
+  return str.replace(regex, "");
+}
+function isInvalidInput(str){
+  const regex = /\d+e\d+/i;
+}
